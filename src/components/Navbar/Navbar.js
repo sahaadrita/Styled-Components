08cfc965186788cce98321a8835ac79a71b794ac
sahaadrita <adrita.saha@optimizeitsystems.com>
2021-10-19 21:27:43 +0530

    day1

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Nav} from './Navbar.elements'
+
+const Navbar = () => {
+    return (
+        <>
+         <Nav>
+             Test
+         </Nav>
+        </>
+    )
+}
+
+export default Navbar;
